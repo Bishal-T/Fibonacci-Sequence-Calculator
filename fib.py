299f48ede7950f65033f586ec4d6540377ff4f53
Bishal <bthapa3@uncc.edu>
2022-09-27 22:39:35 -0400

    Fib Calculator

---
+++ fib.py
@@ -0,0 +1,29 @@
+#Worked with Alex, Submehs, Emre, Melvin 
+
+from random import *
+import time 
+
+first_term = 0
+second_term = 1
+nth_term = randint(15,35)
+
+count = 0
+
+start_time = time.time()
+
+while count < nth_term:
+
+    next_term = first_term + second_term
+    first_term = second_term 
+    second_term = next_term
+    count += 1
+
+
+end_time = time.time()
+
+total_time = (end_time - start_time) 
+
+print(f'fib({nth_term})= {first_term}')
+
+print(f'fib({nth_term}) took {total_time} seconds')
+
